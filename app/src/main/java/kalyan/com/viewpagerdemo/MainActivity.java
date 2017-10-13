689145949e30e544b975e7cc08023558a979102e
Kalyan Raghu <nckraghu@gmail.com>
2017-10-13 19:04:54 +0530

    Added PagerTitleStrip

--- app/src/main/java/kalyan/com/viewpagerdemo/MainActivity.java
+++ app/src/main/java/kalyan/com/viewpagerdemo/MainActivity.java
@@ -1,9 +1,11 @@
 package kalyan.com.viewpagerdemo;
 
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 
 import kalyan.com.viewpagerdemo.views.ViewPagerAdapter;
 
@@ -16,6 +18,11 @@ public class MainActivity extends FragmentActivity {
 
 
         ViewPager viewPager = findViewById(R.id.view_pager);
+        PagerTitleStrip pagerTitleStrip = findViewById(R.id.pager_title_strip);
+
+        pagerTitleStrip.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+        pagerTitleStrip.setTextColor(getResources().getColor(R.color.white, getTheme()));
+        pagerTitleStrip.setBackgroundColor(getResources().getColor(R.color.colorPrimary, getTheme()));
 
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
 
