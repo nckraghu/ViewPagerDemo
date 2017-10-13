e7b8f80f831321d2072159eecddda6cf2732b2ce
Kalyan Raghu <kalyan.raghu@gmail.com>
2017-10-13 13:26:03 +0530

    Added ViewPager fragments

--- app/src/main/java/kalyan/com/viewpagerdemo/MainActivity.java
+++ app/src/main/java/kalyan/com/viewpagerdemo/MainActivity.java
@@ -1,13 +1,24 @@
 package kalyan.com.viewpagerdemo;
 
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import kalyan.com.viewpagerdemo.views.ViewPagerAdapter;
+
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(viewPagerAdapter);
     }
 }
